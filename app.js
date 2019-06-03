d05da74061de69b2a0ab9ecaff611cb47fe6e97b
J-S-Lee <jasoc1004@gmail.com>
2019-06-03 22:04:25 +0900

    connect to mongoDB and make model, upload video.

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { localsMiddleware } from './middlewares';
+import routes from './routes';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
 const app = express();
 
+app.use(helmet());
 app.set('view engine', 'pug');
+app.use('/uploads', express.static('uploads'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan('dev'));
+app.use(localsMiddleware);
 
 app.use('/', globalRouter);
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 export default app;
