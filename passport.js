a9c916a8ba764fd76136b151c359b85c92830144
J-S-Lee <jasoc1004@gmail.com>
2019-06-14 14:10:25 +0900

    User Authentication by local, github, facebook.

---
+++ passport.js
@@ -0,0 +1,40 @@
+import passport from 'passport';
+import GithubStrategy from 'passport-github';
+import FacebookStrategy from 'passport-facebook';
+import User from './models/User';
+import {
+  githubLoginCallback,
+  facebookLoginCallback
+} from './controllers/userController';
+import routes from './routes';
+
+passport.use(User.createStrategy());
+
+// Github
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: `http://localhost:4000${routes.githubCallback}`
+    },
+    githubLoginCallback
+  )
+);
+
+// Facebook
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+      scope: ['public_profile', 'email']
+    },
+    facebookLoginCallback
+  )
+);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
